38bda30cddc8fafa36737c2c20f5967b2785b688
not-damien <93546644+not-damien@users.noreply.github.com>
2023-06-17 23:49:55 -0700

    getting started

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import rocket from './rocket.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div id="tittle-container">
+        <h1 id="site-name">Cynthia Chand</h1>
+        <div id="sub-tittle">
+          <h2>Designing the Future.</h2>
+          <img src={rocket} className="App-logo" alt="logo" />
+        </div>
+        </div>
+
+      
+          <div className='links'>
+          <a>Projects</a> <a>Resume</a> <a href="https://www.linkedin.com/in/cynthia-chand-976495171/">Linkedin</a> <a>about</a>
+          </div>
       </header>
+     
+
+     <Card></Card>
     </div>
   );
 }
 
+
+
+function Card() {
+  return(
+    <div className='card-container'>
+      <h3>
+        simple tee
+      </h3>
+      <p>
+        this is a short paragraph about tee
+      </p>
+    </div>
+  )
+}
 export default App;
