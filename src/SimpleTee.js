983b05ee17630d377b6832dfcb11ca7709f2f035
not-damien <93546644+not-damien@users.noreply.github.com>
2023-08-02 23:00:55 -0700

    added photos

--- src/SimpleTee.js
+++ src/SimpleTee.js
@@ -1,4 +1,13 @@
 import "./Research.css"
+
+import paper1 from "./images/simpleteepaper1.png"
+import paper2 from "./images/simpleteepaper2.png"
+import digital1 from "./images/simpleteedigital1.png"
+import digital2 from "./images/simpleteedigital2.png"
+
+
+
+
 function SimpleTee(){
     return (
         <div>
@@ -32,11 +41,31 @@ function SimpleTee(){
                 <h2>Initial Design Concept</h2>
                 <p>My goal here was to create a home screen with buttons for each customization option, along with a preview of the shirt being customized, and a way to zoom in on the screen.</p>
             </div>
+            <div className="side-by-side">
+                <img className="img-on-page" src={paper1}/>
+                <img src={paper2}/>
+            </div>
+            
+            
             <div className="center">
                 <h2>Low Fidelity Prototype</h2>
                 <p>The low fidelity prototype connected the user flow of designing and purchasing a custom t-shirt in the app.</p>
                 <p>View the SimpleTee <a target="blank" href="https://www.figma.com/file/iGnLNx9T2BoNnUiXdkwmDX/Low-Fidelity-Prototype-v.1?type=design&node-id=0-1&mode=design&t=Q0Ait8yw5OWjxVaR-0">low fidelity prototype</a></p>
             </div>
+            <div className="center">
+                <h2>Digital Wireframes</h2>
+                <p>I took my final paper wireframes and cleaned them up by rearranging some elements for a more user friendly design.</p>
+            </div>
+            <div className="side-by-side">
+                <img className="img-on-page" src={digital1}/>
+                <img src={digital2}/>
+            </div>
+
+
+
+
+
+
            <section>
             <p>User Testing Results:  Users want a way to adjust text, hover over images to view them larger, and want the names of the colors written below them. They also wanted a storefront with premade tees, and a way to go back and edit the items in their cart.</p>
             <div className="center">
