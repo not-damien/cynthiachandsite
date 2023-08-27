a2cde7eb278499ab46524c1c4e8b9edfdef0e11d
not-damien <93546644+not-damien@users.noreply.github.com>
2023-08-27 01:04:14 -0700

    added content for foldhere page

--- src/Foldhere.js
+++ src/Foldhere.js
@@ -2,20 +2,23 @@ import "./Research.css"
 export default function Foldhere(){
     return (
         <div>
-             <div className="center"> 
+            <h1>
+                Welcome to FoldHere! 
+            </h1>
+            <h3>
+                File Sharing Made Simple
+            </h3>
+            <div className="center"> 
             <div className="outline">
                 <p>My role: UX Designer and Researcher</p>
                 <p>
-                Project Goal:
+                Project Goal: The goal was to create a desktop widget that would allow for users to easily share files between computers without the restriction of methods such as sending emails. 
                 </p>
-                Target Audience:
+                Target Audience: People who work in collaboration with others and need a way to securely share files.
                 <p>
-                Challenges:
+                Challenges: Keeping the design simple and easy to understand while also having enough options for more experienced users.
                 </p>
-                Research Summary:
-                <p>
-                Upon completion of the research we concluded that 
-                </p>
+                Research Summary: Users find that their options are limited which in turn actually makes the widget more confusing to use. The design has been reevaulated to account for this addition.
             </div>
             </div>
 
--- src/GameAssets.js
+++ src/GameAssets.js
@@ -1,4 +1,5 @@
 import "./GameAssets.css"
+import "./Research.css"
 import bread from './images/bread 1.png'
 import chainsaw from './images/chainsaw 1.png'
 import dog from './images/doge 1.png'
@@ -8,15 +9,12 @@ function GameAssets(){
     
     <div>
     <h1>Assorted Game Assets</h1>
-    <div className="center"> 
-        <div className="border-div">
+    <div className="center background"> 
             <img src={bread}/>
-            
-            <img src={dog}/>
+            <img src={chainsaw}/>
             <p>Created for a game inspired by Flappy Bird and the anime Chainsaw Man made by Damien Cruz, 2023</p>
             <p><a href="https://not-damien.github.io/FlappyBirdClone/" target="blank">Click here to play it!</a></p>
-            <img src={chainsaw}/>
-        </div>
+            <img src={dog}/>
     </div>
     </div>
        
